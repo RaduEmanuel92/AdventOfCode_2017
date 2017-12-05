01fb3ac54b9391f567a3812a330ebf9fc63427c2
RaduEmanuel92 <chiscariuradu@gmail.com>
2017-12-06 01:54:48 +0200

    Added task3

--- day3/challenge3.rb
+++ day3/challenge3.rb
@@ -17,7 +17,7 @@ class Challenge3
 
 	def find_lower_bound_square(value_=@value)
 		search_ = (value_).to_i
-		(2..search_).each { |x| return x-1 if x**2 >= @value}
+		(1..search_).each { |x| return x-1 if x**2 >= @value}
 	end
 	
 	def bound(index)
@@ -42,7 +42,7 @@ class Challenge3
 		end		
 	end
 
-	def compute_position(index, value_=value)
+	def compute_position(index, value_=@value)
 		bound_ = bound(index)
 		if bound_ == 2
 			index1_ = index ** 2
@@ -68,37 +68,82 @@ class Challenge3
 	def getManhattanDistance(index_start, index_target)
 		return ((index_start[0] - index_target[0]).abs + (index_start[1] - index_target[1]).abs)
 	end
-
-	def compute_task2(index_target)
-		sum = 	m[index_target[0]-1][index_target[1]-1] +
-				m[index_target[0]][index_target[1]-1] 	+
-				m[index_target[0]+1][index_target[0]-1] +
-				m[index_target[0]-1][index_target[0]] 	+
-				m[index_target[0]-1][index_target[0]+1] +
-				m[index_target[0]+1][index_target[0]+1] +
-				m[index_target[0]][index_target[0]+1] 	+
-				m[index_target[0]-1][index_target[0]+1]
+	def neib_sum(m,i,j)
+		sum = 	m[i-1][j-1].to_i +
+				m[i][j-1].to_i 	+
+				m[i+1][j-1].to_i +
+				m[i+1][j].to_i 	+
+				m[i+1][j+1].to_i +
+				m[i][j+1].to_i +
+				m[i-1][j+1].to_i +
+				m[i-1][j].to_i
 		return sum
 	end
 
+	def spiral(n)
+  		spiral = Array.new(n) {Array.new(n, 0)}     
+  		runs = 0.upto(n).each_cons(2).to_a.flatten  # n==5; [5,4,4,3,3,2,2,1,1,0]
+  		delta = [[1,0], [0,1], [-1,0], [0,-1]].cycle
+  		x, y, value = n/2, n/2, 1
+  		spiral[n/2][n/2] = 1
+  		for run in runs
+    		dx, dy = delta.next
+    		run.times { spiral[x+=dx][y+=dy] = (value+=1) }
+ 		end
+  		spiral
+	end
+
+	def print_matrix(m)
+		width = m.flatten.map { |x| x.to_s.size }.max 
+		m.each{ |row| puts row.map { |x| "%#{width}s " % x }.join}
+	end
+
+
+	def spiral2(n, stop_val)
+  		spiral = Array.new(n) {Array.new(n, 0)}   
+  		runs = 0.upto(n).each_cons(2).to_a.flatten  
+  		delta = [[1,0], [0,1], [-1,0], [0,-1]].cycle
+  		x, y, value = n/2, n/2, 1
+  		spiral[n/2][n/2] = 1
+  		for run in runs
+    		dx, dy = delta.next
+    		run.times { 
+      			if ((x == n/2) && (y == n/2)) 
+        			#spiral[x+=dx][y+=dy] =  1
+        			spiral[x+=dx][y+=dy] = 1
+      			else
+        			value = neib_sum(spiral,x+=dx,y+=dy)
+        			#printf("value %s\n", value)
+        			spiral[x][y] = value
+        			if value > stop_val
+          				return value
+          				break
+        			end
+      			end
+     		#print_matrix spiral 
+      		} 
+  		end
+  		spiral
+  
+	end
 end
 
 
 if __FILE__ == $0
-	#input = 265149
+	input = 265149
 	#input = 1024
-	input = 10
+	#input = 31
 
 	challenge3 	= Challenge3.new(input)
 	size_m 		= challenge3.find_lower_bound_square(input)
 	pos1 		= challenge3.get_index_of_start_spiral(size_m)
 	pos_target 	= challenge3.compute_position(size_m, input)
+	dist = challenge3.getManhattanDistance(pos1, pos_target)
 	printf("%s\n", pos_target)
-	dist        = challenge3.getManhattanDistance(pos1, pos_target)
-	#puts dist
+	printf("Distance: %s\n", dist)
 
 	#Task2
 
-
-
+	task2 = challenge3.spiral2(size_m, input) 
+	puts task2
 end
