2c12c1c0a4a48691a4af0a9232c9138060a33d25
RaduEmanuel92 <chiscariuradu@gmail.com>
2017-12-10 11:49:50 +0200

    updated task6

---
+++ day6/challenge6.rb
@@ -0,0 +1,48 @@
+#!/usr/bin/env ruby
+
+require "solid_assert"
+
+$database = []
+
+def parse_input() 
+		input_list 	= []
+		file 		= File.open('input', 'r') {|f| f.read}
+		input_list 	= file.split("\t").map(&:to_i)		
+		return input_list
+end
+
+def redistribute(input_list)
+		max_index 	= input_list.index(input_list.max)
+		steps 	  	= input_list[max_index]
+		input_list[max_index] = 0
+		for x in (1..steps)
+			input_list[(max_index + x) % input_list.length] += 1
+		end
+	return	input_list
+end
+
+def count_interations(input_list) 
+	candidate_list	= input_list
+	steps = 0
+	while true
+		candidate_list 	= redistribute(candidate_list)
+		steps += 1
+		#printf("%s\n",candidate_list)
+		search_list = candidate_list.join(',')
+		if $database.include? search_list
+		 	break
+		else
+		 	$database.push(search_list)	
+		end
+	end
+	return steps
+end
+
+if __FILE__ == $0
+
+	input_l	= parse_input()
+	$database.push(input_l.join(','))
+	test_array = [0, 2, 7, 0]
+	assert count_interations(test_array) == 5, "[-] Test failed."
+	puts count_interations(input_l)
+end
