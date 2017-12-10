f4b34d06a72c4eaafb36be0606a87e938fca81c3
RaduEmanuel92 <chiscariuradu@gmail.com>
2017-12-10 12:13:50 +0200

    updated task6

--- day6/challenge6.rb
+++ day6/challenge6.rb
@@ -15,36 +15,49 @@ def redistribute(input_list)
 	max_index 	= input_list.index(input_list.max)
 	steps 	  	= input_list[max_index]
 	input_list[max_index] = 0
+
 	for x in (1..steps)
 		input_list[(max_index + x) % input_list.length] += 1
 	end
 	return	input_list
 end
 
-def count_interations(input_list) 
+def count_interations(input_list)
+	#initialize database
+	$database = []
+	$database.push(input_list.join(',')) 
 	candidate_list	= input_list
-	steps = 0
+	steps 			= 0
+
+	#counter
 	while true
 		candidate_list 	= redistribute(candidate_list)
 		steps += 1
 		#printf("%s\n",candidate_list)
-		search_list = candidate_list.join(',')
-		if $database.include? search_list
+		search_string = candidate_list.join(',')
+		if $database.include? search_string
 		 	break
 		else
-		 	$database.push(search_list)	
+		 	$database.push(search_string)	
 		end
 	end
-	return steps
+	return steps, search_string.split(',').map(&:to_i)
 end
 
 if __FILE__ == $0
 
-	input_l	= parse_input()
-	test_array = [0, 2, 7, 0]
-	#initialize database
-	$database.push(input_l.join(','))
+	input_l		= parse_input()
+	test_array 	= [0, 2, 7, 0]
+
+
+	
 
 	assert count_interations(test_array) == 5, "[-] Test failed."
-	puts count_interations(input_l)
+	
+	result 		= count_interations(input_l)
+	counter 	= result[0]
+	found_config= result[1]
+	puts found_config
+	printf("[+] Loop detected after %s cycles. \n", counter)
+	printf("[+] Size of loop: %s\n", count_interations(found_config)[0])
 end
